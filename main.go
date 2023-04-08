0d3e38a65e289d034787a9776a1cae491c581c34
Michael Willett <51060085+m12t@users.noreply.github.com>
2023-04-07 23:33:46 -0500

    added multi-character and variable length targets, alpha support (lower & upper), and full ascii letters, numbers, symbols

--- main.go
+++ main.go
@@ -1,19 +1,75 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	tb "github.com/nsf/termbox-go"
 )
 
-// TODO: allow more than single digit numbers (opt-in via a CLI flag)
-// 		 allow opt-in letters
-// 	     incorporate some sort of timing
+// TODO: incorporate some sort of timing
+
+//   - we actually need to use unicode since termbox returns unicode.
+//     luckily, the first 127 characters of unicode == first 127 of ASCII
+const (
+	asciiLow, asciiHigh           = 33, 127
+	numLow, numHigh               = 48, 57
+	upperAlphaLow, upperAlphaHigh = 65, 90
+	lowerAlphaLow, lowerAlphaHigh = 97, 122
+)
+
+func generateTarget(bank *[]rune, targetLen int, isVariableLen bool) ([]rune, int) {
+	if isVariableLen {
+		targetLen = rand.Intn(targetLen) + 1
+	}
+	target := make([]rune, 0, targetLen)
+
+	for i := 0; i < targetLen; i++ {
+		target = append(target, (*bank)[rand.Intn(len(*bank))])
+	}
+	return target, len(target)
+}
+
+func buildCharBank(mode, letterCase *string) *[]rune {
+	var bank []rune
+	if *mode == "fullASCII" {
+		for i := asciiLow; i <= asciiHigh; i++ {
+			bank = append(bank, rune(i))
+		}
+		return &bank
+	}
+	if *mode == "alpha" || *mode == "alphanum" {
+		if *letterCase == "lower" || *letterCase == "mixed" {
+			for i := lowerAlphaLow; i <= lowerAlphaHigh; i++ {
+				bank = append(bank, rune(i))
+			}
+		}
+		if *letterCase == "upper" || *letterCase == "mixed" {
+			for i := upperAlphaLow; i <= lowerAlphaHigh; i++ {
+				bank = append(bank, rune(i))
+			}
+		}
+	}
+	if *mode == "num" || *mode == "alphanum" {
+		for i := numLow; i <= numHigh; i++ {
+			bank = append(bank, rune(i))
+		}
+	}
+	return &bank
+}
 
 func main() {
+
+	modePtr := flag.String("mode", "num", "mode: {'alpha', 'num', 'alphanum', 'fullASCII'}")
+	casePtr := flag.String("case", "mixed", "case: {'lower', 'upper', 'mixed'}")
+	maxLenPtr := flag.Int("length", 1, "max target string length (or target string length if `variable` flag is False)")
+	variablePtr := flag.Bool("variable", true, "generate variable length target string for each run over range [1, length]")
+	flag.Parse()
+
+	charBank := buildCharBank(modePtr, casePtr)
+
 	err := tb.Init()
 	if err != nil {
 		panic(err)
@@ -29,23 +85,28 @@ func main() {
 	tb.PollEvent()
 
 	rand.Seed(time.Now().UnixNano())
-	var hits, total int
-	target := strconv.Itoa(rand.Intn(10))
+	var idx, hits, total int
+	target, size := generateTarget(charBank, *maxLenPtr, *variablePtr)
 	for {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(50 * time.Millisecond)
 		fmt.Print("\033[H\033[2J") // clear the terminal
-		fmt.Println(target)
+		fmt.Println(string(target))
 		event := tb.PollEvent() // blocking
 		if event.Key == tb.KeyEsc {
 			fmt.Print("\033[H\033[2J")
-			fmt.Printf("%.2f accuracy %d/%d\n", float64(hits)/float64(total)*100.0, hits, total)
+			fmt.Printf("%.2f accuracy %d/%d characters\n", float64(hits)/float64(total)*100.0, hits, total)
 			time.Sleep(5 * time.Second) // give the user time to view the results before exiting.
 			return
 		}
 		total++
-		if string(event.Ch) == target {
+		if event.Ch == target[idx] {
 			hits++
-			target = strconv.Itoa(rand.Intn(10))
+			idx++
+			if idx == size {
+				// the full target has been matched. Reset the idx to 0 and generate a new target
+				target, size = generateTarget(charBank, *maxLenPtr, *variablePtr)
+				idx = 0
+			}
 		} else {
 			fmt.Println("MISS!")
 		}
